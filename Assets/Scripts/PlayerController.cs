001fd67d491cd4017f76ddfd604765a596ed2083
Michael Victorino <michaelvictorino@gmail.com>
2023-12-11 21:17:51 -0500

    Changed character joints to configurable joints

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public float Speed;
-    public float StrafeSpeed;
-    public float JumpForce;
+    [SerializeField]
+    private float Speed, StrafeSpeed, JumpForce;
+    [SerializeField]
+    private Rigidbody Hips;
+    [SerializeField]
+    private bool IsGrounded;
 
-    public Rigidbody Hips;
-    public bool IsGrounded;
+    private PlayerInputActions PlayerInput;
 
+    private InputAction Move;
+
+    Vector2 MoveDir;
+
+    private void Awake()
+    {
+        PlayerInput = new PlayerInputActions();        
+    }
     void Start()
     {
-        Hips = GetComponent<Rigidbody>();   
+        Hips = GetComponent<Rigidbody>();
+
+
     }
-    private void FixedUpdate()
+
+    private void OnEnable()
+    {
+        Move = PlayerInput.Player.Move;
+        Move.Enable();
+    }
+    private void OnDisable()
+    {
+        Move.Disable();
+    }
+
+    private void MovePlayer()
     {
     }
+
+    private void Update()
+    {
+        MoveDir = Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Hips.AddForce(-Hips.transform.forward * MoveDir.y * Speed, ForceMode.Force);
+        Hips.AddForce(Hips.transform.right * MoveDir.x * StrafeSpeed);
+
+
+        //Vector3 moveDir3 = MoveDir;
+        //Hips.MovePosition(Hips.transform.forward + moveDir3 * Time.deltaTime * Speed);
+
+        Debug.Log(MoveDir.y);
+
+    }
+
 
 }
